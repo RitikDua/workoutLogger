a7c7f4f68536b21dc3b8cbda09fb35185e153858
RitikDua <rittikdua@gmail.com>
2020-06-03 13:51:56 +0530

    Services updated

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -14,15 +14,22 @@ export class UserService {
 	exercisesList:ExerciseList;
 	exercise:EXERCISE;
 
+	exercieseWithTime:string[]=["running","jogging","walking","yoga"];
 	weeks:string[]=[
-	"monday","tuesday","wednesday","thursday","friday","saturday","sunday"
+	"sunday","monday","tuesday","wednesday","thursday","friday","saturday"
 	];
 	constructor() { 
 		this.user={username:"Ritik",
 					exercisesList:[],
 					stats:[]
 				};
-		
+
+		this.user.stats.push({
+			date:(new Date()).toString().slice(0,15),
+			weight:0,
+			height:0,
+			todayStats:[],
+		})
 		this.printData();
 	}
 	addExercises(day:string,exerciseList:string[]):void{
@@ -31,23 +38,31 @@ export class UserService {
 		
 		this.printData();
 	}
+	//it will update one exercise
+	//but if a exercise is added and sets and reps are added 
+	//you cannot del this for now
 	updateOneExercise(i:number,exerciseName:string,exercise:EXERCISE):void{
 		this.user.stats[i].todayStats[exerciseName]=exercise;
 
 		this.printData();
 	}
-	addStatsOfOneExercise(date:Date,exerciseName:string,exercise:EXERCISE):void{
+	addStatsOfOneExercise(date:string,exerciseName:string,exercise:EXERCISE):void{
 		for(let i=0;i<this.user.stats.length;i++)
 		{
 			if(this.user.stats[i].date===date){
-				this.updateOneExercise(i,exerciseName,exercise);
+				this.updateOneExercise(i,exerciseName.toLowerCase(),exercise);
 				break;
 			}
 		}
 
 		this.printData();
 	}
-	addStatsOfExercises(date:Date,weight:number,height:number,todayStats:OneDayStats[]):void{
+	//exercise which required the mins and hrs
+	isTime(exerciseName:string):boolean{
+		if(this.exercieseWithTime.indexOf(exerciseName)>-1) return true;
+		return false;
+	}
+	addStatsOfExercises(date:string,weight:number,height:number,todayStats:OneDayStats[]):void{
 		for(let i=0;i<this.user.stats.length;i++)
 		{
 			if(this.user.stats[i].date===date){
@@ -61,7 +76,7 @@ export class UserService {
 	}
 	getExercisesList():ExerciseList{
 		let day=(new Date()).getDay();
-		
+		console.log(day);
 		this.printData();
 		return this.user.exercisesList[this.weeks[day]];
 	}
