a22546c97798ad9bce835e84cb0f3f402a7a5d67
RitikDua <rittikdua@gmail.com>
2020-06-04 19:33:19 +0530

    stats routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,20 @@ import { Routes, RouterModule } from '@angular/router';
 import {AddComponent} from './components/add/add.component';
 import {StatsComponent} from './components/stats/stats.component';
 import {ProfileComponent} from './components/profile/profile.component';
-
+import { LineChartComponent } from './components/stats/line-chart/line-chart.component';
+import { DoughnutChartComponent } from './components/stats/doughnut-chart/doughnut-chart.component';
+import { BarChartComponent } from './components/stats/bar-chart/bar-chart.component';
+import { PieChartComponent } from './components/stats/pie-chart/pie-chart.component';
+import { RadarChartComponent } from './components/stats/radar-chart/radar-chart.component';
 const routes: Routes = [
 	{path:"",redirectTo:"/add",pathMatch:'full'},
-	{path:'stats',component:StatsComponent},
+	{path:'stats',component:StatsComponent,
+	children:[
+	{path:'overall',component:LineChartComponent},
+	{path:'week',component:BarChartComponent},
+	{path:'today',component:PieChartComponent},
+	]
+},
 	{path:'profile',component:ProfileComponent},
 	{path:'add',component:AddComponent},	
 ];
